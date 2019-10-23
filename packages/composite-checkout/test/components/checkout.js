768c7ef82adef29eb23507b87302fcd07ce497ac
nbloomf <nathan.bloomfield@automattic.com>
2019-10-23 16:43:16 -0500

    Add basic test using react-testing-library

---
+++ packages/composite-checkout/test/components/checkout.js
@@ -0,0 +1,48 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { render, getByText } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import Checkout from '../../src/components/checkout';
+
+test( 'When we enter checkout, the line items and total are rendered', () => {
+	const { container } = render(
+		<Checkout
+			locale="en-us"
+			items={ [
+				{
+					label: 'Illudium Q-36 Explosive Space Modulator',
+					id: 'space-modulator',
+					type: 'widget',
+					amount: { currency: 'USD', value: 5500, displayValue: '$55' },
+				},
+			] }
+			total={ {
+				label: 'Total',
+				id: 'total',
+				type: 'total',
+				amount: { currency: 'USD', value: 5500, displayValue: '$55' },
+			} }
+			onSuccess={ () => {
+				console.log( 'Success :)' );
+			} }
+			onFailure={ () => {
+				console.log( 'Failure :(' );
+			} }
+			successRedirectUrl="#"
+			failureRedirectUrl="#"
+		/>
+	);
+
+	const totalDiv = container.querySelector( '.order-review-total' );
+	expect( getByText( totalDiv, 'Total' ) ).toBeDefined();
+	expect( getByText( totalDiv, '$55' ) ).toBeDefined();
+
+	const lineItemsDiv = container.querySelector( '.order-review-line-items' );
+	expect( getByText( lineItemsDiv, 'Illudium Q-36 Explosive Space Modulator' ) ).toBeDefined();
+	expect( getByText( lineItemsDiv, '$55' ) ).toBeDefined();
+} );
